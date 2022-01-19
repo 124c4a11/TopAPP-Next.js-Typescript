1b9036fcc7cb7ef27b73e5763eded9c1f768e0f3
124c4a11 <124c4a11@mail.ru>
2022-01-19 18:53:43 +0300

    Button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import cn from 'classnames';
+
+import styles from './Button.module.css';
+import { IButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
+
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: IButtonProps): JSX.Element => {
+  const btnClassName = cn(
+    styles.btn,
+    className,
+    {
+      [styles['btn-primary']]: appearance === 'primary',
+      [styles['btn-ghost']]: appearance === 'ghost',
+    }
+  );
+
+  const arrowClassName = cn(
+    styles.arrow,
+    {
+      [styles['arrow-down']]: arrow === 'down',
+    }
+  );
+
+  return <button className={btnClassName} {...props}>
+    {children}
+    {arrow !== 'none' && <span className={arrowClassName}><ArrowIcon /></span>}
+  </button>;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import { Htag } from "../components";
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
-    <Htag tag='h1'>Hedline 1</Htag>
+    <>
+      <Htag tag="h1">Hedline 1</Htag>
+      <Button appearance="primary" arrow="right">Button</Button>
+      <Button appearance="ghost" arrow="down">Button</Button>
+    </>
   );
 }
