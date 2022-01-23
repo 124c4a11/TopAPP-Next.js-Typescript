b6909212c71eeaa46ffbbdd1f4b9a7f85df8f2ee
124c4a11 <124c4a11@mail.ru>
2022-01-23 16:45:29 +0300

    Layout: styles

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,19 +1,18 @@
 import { FunctionComponent } from 'react';
 
+import styles from './Layout.module.css';
 import { ILayoutProps } from './Layout.props';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 
 const Layout = ({ children }: ILayoutProps): JSX.Element => (
-  <>
-    <Header></Header>
-    <div>
-      <Sidebar />
-      <main>{children}</main>
-    </div>
-    <Footer></Footer>
-  </>
+  <div className={styles.container}>
+    <Header className={styles.header} />
+    <Sidebar className={styles.sidebar} />
+    <main className={styles.main}>{children}</main>
+    <Footer className={styles.footer}></Footer>
+  </div>
 );
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
