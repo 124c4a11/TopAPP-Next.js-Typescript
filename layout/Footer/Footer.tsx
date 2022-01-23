edb7148cf21e932a224e0b063e64eefd243e8a84
124c4a11 <124c4a11@mail.ru>
2022-01-23 17:39:04 +0300

    Footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,5 +1,14 @@
-import { IFooterProps } from './Footer.props';
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = (props: IFooterProps): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
-};
+import styles from './Footer.module.css';
+import { IFooterProps } from './Footer.props';
+import { P } from '../../components';
+
+export const Footer = ({ className, ...props }: IFooterProps): JSX.Element => (
+  <footer className={cn(className, styles.footer)} {...props}>
+    <P>OwlTop © 2020 - {format(new Date, 'yyyy')} Все права защищены</P>
+    <a href="#" target="_blank">Пользовательское соглашение</a>
+    <a href="#" target="_blank">Политика конфиденциальности</a>
+  </footer>
+);
