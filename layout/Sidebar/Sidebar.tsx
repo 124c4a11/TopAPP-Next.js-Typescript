9d782447a90126ec657fe934b8fd4d7a223e52e9
124c4a11 <124c4a11@mail.ru>
2022-01-27 19:49:07 +0300

    Sidebar: styles

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,6 +1,16 @@
+import cn from 'classnames';
+
+import styles from './Sidebar.module.css';
+import Logo from '../logo.svg';
 import { Menu } from '../Menu/Menu';
 import { ISidebarProps } from './Sidebar.props';
 
-export const Sidebar = (props: ISidebarProps): JSX.Element => {
-  return <aside {...props}><Menu /></aside>;
+export const Sidebar = ({ className, ...props }: ISidebarProps): JSX.Element => {
+  return (
+    <aside className={cn(className, styles.sidebar)} {...props}>
+      <Logo className={styles.logo} />
+      <div>Search</div>
+      <Menu />
+    </aside>
+  );
 };
