71ed5b2c401ebc85c3ecd74f789f0a5b35baf559
124c4a11 <124c4a11@mail.ru>
2022-01-30 15:22:51 +0300

    TopPageComponent: section

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames';
 import parse from 'html-react-parser';
 
 import styles from './TopPageComponent.module.css';
-import { Advantages, H, HhData, P, Tag, TagList } from '../../components';
+import { Advantages, H, HhData, Tag, TagList } from '../../components';
 import { ITopPageComponentProps } from './TopPageComponent.props';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 
@@ -11,38 +11,41 @@ export const TopPageComponent = ({ page, products, firstCategory }: ITopPageComp
     <>
       <header className={styles.header}>
         <H>{page.title}</H>
-        {products && <Tag color="gray">{products.length}</Tag>}
+        {products && <Tag color='gray'>{products.length}</Tag>}
         <div>Сортировка</div>
       </header>
-      <div>
+      <div className={styles['section']}>
         {products && products.map((product) => <div key={product._id}>{product.title}</div>)}
       </div>
       {
         firstCategory === TopLevelCategory.Courses && page.hh &&
-        <>
-          <div className={cn(styles["hh-title"], styles["section-title"])}>
-            <H tag="h2">Вакансии - {page.category}</H>
-            <Tag color="red">hh.ru</Tag>
-          </div>
+        <section className={styles['section']}>
+          <header className={cn(styles['hh-title'], styles['section-title'])}>
+            <H tag='h2'>Вакансии - {page.category}</H>
+            <Tag color='red'>hh.ru</Tag>
+          </header>
           <HhData {...page.hh} />
-        </>
+        </section>
       }
       {
         page.advantages && page.advantages.length > 0 &&
-        <>
-          <H tag="h2" className={styles['section-title']}>Преимущества</H>
+        <section className={styles['section']}>
+          <H tag='h2' className={styles['section-title']}>Преимущества</H>
           <Advantages advantages={page.advantages} />
-        </>
+        </section>
       }
       {
-        page.seoText && parse(page.seoText)
+        page.seoText &&
+        <section className={styles['section']}>
+          {parse(page.seoText)}
+        </section>
       }
       {
         page.tags &&
-        <>
-          <H tag="h2" className={styles['section-title']}>Получаемые навыки</H>
+        <section className={styles['section']}>
+          <H tag='h2' className={styles['section-title']}>Получаемые навыки</H>
           <TagList tags={page.tags} />
-        </>
+        </section>
       }
     </>
   );
