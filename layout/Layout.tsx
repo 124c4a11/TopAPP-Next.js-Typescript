ee767e75dc7cc2a55998c357b34bcccff8f9e3e7
124c4a11 <124c4a11@mail.ru>
2022-02-07 20:24:55 +0300

    Accessibility: skip link

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, KeyboardEvent, useRef } from 'react';
 
 import styles from './Layout.module.css';
 import { AppContextProvider, IAppContext } from '../context/app.context';
@@ -7,14 +7,35 @@ import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
 
-const Layout = ({ children }: ILayoutProps): JSX.Element => (
-  <div className={styles.container}>
-    <Header className={styles.header} />
-    <Sidebar className={styles.sidebar} />
-    <main className={styles.main}>{children}</main>
-    <Footer className={styles.footer}></Footer>
-  </div>
-);
+const Layout = ({ children }: ILayoutProps): JSX.Element => {
+  const mainRef = useRef<HTMLDivElement>(null);
+
+  const skipContentAction = (e: KeyboardEvent) => {
+    if (e.code === 'Space' || e.code === 'Enter') {
+      e.preventDefault();
+
+      mainRef.current?.focus();
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <button
+        tabIndex={1}
+        className={styles['skip-link']}
+        onKeyDown={(e: KeyboardEvent) => skipContentAction(e)}
+      >Сразу к содержимому</button>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <main
+        ref={mainRef}
+        className={styles.main}
+        tabIndex={0}
+      >{children}</main>
+      <Footer className={styles.footer}></Footer>
+    </div>
+  );
+};
 
 export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
   return function withLayoutComponent(props: T): JSX.Element {
