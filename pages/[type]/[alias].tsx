644404108062b50cb645f9301fd9ff35471c988e
124c4a11 <124c4a11@mail.ru>
2022-02-09 17:32:23 +0300

    SEO: meta tags

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import Head from 'next/head';
 import { ParsedUrlQuery } from 'querystring';
 
 import { withLayout } from '../../layout/Layout';
@@ -11,11 +12,22 @@ import { TopPageComponent } from '../../page-components';
 import { API } from '../../helpers/api';
 
 function TopPage({ products, page, firstCategory }: ITopPageProps): JSX.Element {
-  return <TopPageComponent
-    products={products}
-    page={page}
-    firstCategory={firstCategory}
-  />;
+  return (
+    <>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name='description' content={page.metaDescription} />
+        <meta property='og:title' content={page.metaTitle} />
+        <meta property='og:description' content={page.metaDescription} />
+        <meta property='og:type' content='article' />
+      </Head>
+      <TopPageComponent
+        products={products}
+        page={page}
+        firstCategory={firstCategory}
+      />
+    </>
+  );
 }
 
 export default withLayout(TopPage);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>MyTop - наш лучший топ</title>
@@ -10,6 +10,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      <meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN}${router.asPath}`} />
+      <meta property="og:locale" content="ru_Ru" />
     </Head>
     <Component {...pageProps} />
   </>;
